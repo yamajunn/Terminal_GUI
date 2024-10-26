ef13f121c6c63d5e256d1d51b57c01a43a003cde
yamajunn <amiton1115@gmail.com>
2024-10-26 16:10:58 +0900

    Add GUI functionality to track mouse position and button clicks in terminal window

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ from pynput import mouse
 import subprocess
 import shutil
 import os
+import time
 
 # AppleScriptを用いてターミナルウィンドウの位置とサイズを取得
 script = '''
@@ -22,12 +23,36 @@ terminal_size = shutil.get_terminal_size()
 
 count = 0
 
-terminal_str_x = 0
-terminal_str_y = 0
+terminal_cursor_x = 0
+terminal_cursor_y = 0
+
+
+def button():
+    display = [[" " for _ in range(terminal_size.columns)]
+               for _ in range(terminal_size.lines)]
+    display[terminal_size.lines//2][terminal_size.columns //
+                                    2-5:terminal_size.columns//2 + 5] = ["#" for _ in range(10)]
+    for d in display:
+        print("".join(d))
+
+
+def button_press():
+    os.system("clear")
+    display = [[" " for _ in range(terminal_size.columns)]
+               for _ in range(terminal_size.lines)]
+    display[terminal_size.lines//2][terminal_size.columns //
+                                    2-5:terminal_size.columns//2 + 5] = ["_" for _ in range(10)]
+    for d in display:
+        print("".join(d))
+    display[terminal_size.lines//2][terminal_size.columns //
+                                    2-5:terminal_size.columns//2 + 5] = ["#" for _ in range(10)]
+    time.sleep(0.5)
+    for d in display:
+        print("".join(d))
 
 
 def on_move(mouse_x, mouse_y):
-    global x, y, width, height, terminal_size, count, terminal_str_x, terminal_str_y
+    global x, y, width, height, terminal_size, count, terminal_cursor_x, terminal_cursor_y
     if count > 50:
         terminal_size = shutil.get_terminal_size()
 
@@ -36,17 +61,25 @@ def on_move(mouse_x, mouse_y):
             ['osascript', '-e', script], stdout=subprocess.PIPE)
         output = result.stdout.decode().strip()
         x, y, width, height = map(int, output.replace(",", "").split())
-        # os.system("clear")
         count = 0
     count += 1
 
     if x < mouse_x < x + width and y < mouse_y < y + height:
-        terminal_str_x = int((mouse_x - x) / width * terminal_size.columns)
-        terminal_str_y = int((mouse_y - y) / height * terminal_size.lines)
+        terminal_cursor_x = int((mouse_x - x) / width * terminal_size.columns)
+        terminal_cursor_y = int((mouse_y - y) / height * terminal_size.lines)
     else:
-        terminal_str_x = 0
-        terminal_str_y = 0
+        terminal_cursor_x = 0
+        terminal_cursor_y = 0
+
+# マウスのクリックを監視
 
 
-with mouse.Listener(on_move=on_move) as listener:
+def on_click(x, y, button, pressed):
+    if pressed and terminal_size.columns//2-5 <= terminal_cursor_x <= terminal_size.columns//2+5 and terminal_size.lines//2-2 <= terminal_cursor_y <= terminal_size.lines//2:
+        button_press()
+
+
+button()
+
+with mouse.Listener(on_move=on_move, on_click=on_click) as listener:
     listener.join()
