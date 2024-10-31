126df910dd00028288cca8e3005753e64dfbf4a6
yamajunn <amiton1115@gmail.com>
2024-10-31 22:16:13 +0900

    Refactor button function to improve fullwidth character handling and update display_creater to use '#' for initialization; remove tempCodeRunnerFile.py

--- button.py
+++ button.py
@@ -2,12 +2,12 @@ import is_fullwidth
 
 
 def button(b, display):
+    count = 0
+    for d in display[b[1]]:
+        if is_fullwidth.is_fullwidth(d):
+            count += 1
     for i, s in enumerate(b[2]):
-        count = 0
-        for d in display[b[1]]:
-            if is_fullwidth.is_fullwidth(d):
-                count += 1
         if is_fullwidth.is_fullwidth(s):
-            display[b[1]].pop(b[0]+i)
+            display[b[1]].pop(b[0]+i+10)
         display[b[1]][b[0]+i-count] = s
     return display
