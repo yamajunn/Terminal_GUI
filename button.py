6f64b2a30f2fac4b15cad66e24976d663c7e2bf5
yamajunn <amiton1115@gmail.com>
2024-10-31 11:06:44 +0900

    Refactor button function to use pop method for clarity and update display_creater to initialize display with dashes

--- button.py
+++ button.py
@@ -4,6 +4,6 @@ import is_fullwidth
 def button(b, display):
     for i, s in enumerate(b[2]):
         if is_fullwidth.is_fullwidth(s):
-            del display[b[1]][b[0]+i]
+            display[b[1]].pop(b[0]+i)
         display[b[1]][b[0]+i] = s
     return display
--- display_creater.py
+++ display_creater.py
@@ -6,13 +6,12 @@ import is_fullwidth
 
 def display_creater(terminal_size, folder_path):
     folder_path_list = folder_path_split.folder_path_split(folder_path)[-4:]
-    display = [[" " for _ in range(terminal_size.columns)]
+    display = [["-" for _ in range(terminal_size.columns)]
                for _ in range(terminal_size.lines)]
     for x, path in enumerate(folder_path_list):
         for b in button_list.button_list(path, terminal_size, x):
             display = button.button(b, display)
-            for d in range(len(display)):
-                display[d][b[0]-5] = "|"
+
     for d in display:
         print("\n"+"".join(d), end="")
     return display
