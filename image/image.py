7d321f0b6abeb67bdf387253bca54dcf97c93031
yamajunn <amiton1115@gmail.com>
2024-11-01 11:11:23 +0900

    画像パスを更新し、アスキー変換の前にターミナルをクリアする機能を追加

--- image/image.py
+++ image/image.py
@@ -1,23 +1,20 @@
 from PIL import Image
 from rich.console import Console
 from rich.text import Text
+import os
 
 # コンソールの初期化
 console = Console()
 
-# 設定する画像のサイズ（例: 幅100ピクセル、高さ50ピクセル）
-new_width = 120
-new_height = 40
-
 # 画像のファイルパスを指定
-image_path = "/Users/chinq500/Desktop/GUI/image2.jpeg"
+image_path = "/Users/chinq500/Desktop/GUI/image5.png"
 image = Image.open(image_path)
-
-original_width, original_height = image.size
 
 # 最大の幅と高さを設定
 max_width = 120
 max_height = 40
+
+original_width, original_height = image.size
 
 # 幅と高さの比率を計算し、比率を維持した最大のサイズを決定
 width_ratio = max_width / original_width
@@ -33,7 +30,7 @@ pixels = resized_image.load()
 
 # アスキー文字のリストとピクセル密度
 chars_list = [('M', 0.037*27), ('@', 0.037*26), ('%', 0.037*25), ('N', 0.037*24), ('B', 0.037*23), ('w', 0.037*22), ('m', 0.037*21), ('6', 0.037*20), ('E', 0.037*19), ('b', 0.037*18), ('4', 0.037*17), ('3', 0.037*16), ('2', 0.037*15),
-              ('k', 0.037*14), ('7', 0.037*13), ('f', 0.037*12), ('1', 0.037*11), ('l', 0.037*10), ('r', 0.037*9), ('i', 0.037*8), ('!', 0.037*7), ('/', 0.037*6), ("'", 0.037*5), ('-', 0.037*4), (',', 0.037*3), ('_', 0.037*2), (' ', 0.037*1)]
+              ('k', 0.037*14), ('7', 0.037*13), ('f', 0.037*12), ('1', 0.037*11), ('l', 0.037*10), ('r', 0.037*9), ('i', 0.037*8), ('!', 0.037*7), ('/', 0.037*6), ("'", 0.037*5), ('-', 0.037*4), (',', 0.037*3), ('_', 0.037*2)]
 
 # ピクセルごとのアスキー文字に変換
 pixel_list = []
@@ -50,6 +47,7 @@ for y in range(new_height):
         line_text.append(closest_char, style=f"rgb({r},{g},{b})")
     pixel_list.append(line_text)
 
+os.system('clear')
 # 結果を表示
 for line in pixel_list:
     console.print(line)
